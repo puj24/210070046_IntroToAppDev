1cd2ea997d2087bf98ae61ad0daf0503e8121a2f
puj24 <pujavedantu24@gmail.com>
2023-07-16 19:55:42 +0530

    Added Week1

---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(CounterApp());
+}
+
+class CounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CounterPage(),
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Counter Value:',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              '$_counter',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Stack(
+        fit: StackFit.expand,
+            children: [
+              Positioned(
+              left: 30,
+              bottom: 20,
+              child: FloatingActionButton(
+                heroTag: 'back',
+                onPressed: _decrementCounter,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Icon(
+                  Icons.remove,
+                  size: 40,
+                ),
+              ),
+            ),
+              Positioned(
+                bottom: 20,
+                right: 30,
+                child: FloatingActionButton(
+                  heroTag: 'next',
+                  onPressed: _incrementCounter,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Icon(
+                    Icons.add,
+                    size: 40,
+                  ),
+                ),
+              ),
+          ],
+      ),
+    );
+  }
+}
+
+/*FloatingActionButton(
+        onPressed: _decrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),*/
